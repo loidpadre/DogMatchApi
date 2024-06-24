083f0c77a05a774729d296a3a3740f16e38344fd
Loid Padre <145937391+loidpadre@users.noreply.github.com>
2024-06-24 20:00:00 +0000

    adicionando o img schemma

--- DogShema.js
+++ DogShema.js
@@ -1,36 +1,37 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const DogSchema = mongoose.Schema({
-    nameOficial:{
-        type: String,
-        require: true
-    },
-    secondaryName:{
-        type: String,
-        require: true
-    },
-    size:{
-        type: String,
-        require: true
-    },
-    country:{
-        type: String,
-        require: true
-    },
-    temperament:{
-        type: String,
-        require: true
-    },
-    history:{
-        type: String,
-        require: true
-    },
-    especificity:{
-        type: String,
-        
-    }
-    
-})
+  nameOficial: {
+    type: String,
+    require: true,
+  },
+  img: {
+    type: String,
+    require: false,
+  },
+  secondaryName: {
+    type: String,
+    require: true,
+  },
+  size: {
+    type: String,
+    require: true,
+  },
+  country: {
+    type: String,
+    require: true,
+  },
+  temperament: {
+    type: String,
+    require: true,
+  },
+  history: {
+    type: String,
+    require: true,
+  },
+  especificity: {
+    type: String,
+  },
+});
 
-
-module.exports = mongoose.model("Dog", DogSchema)
+module.exports = mongoose.model("Dog", DogSchema);
